7b863384fe28f2b8d6ddbafd8295937a6905c3e2
gus-rautenberg <gprautenber@gmail.com>
2024-04-19 15:31:52 -0300

    Adding Boot Record Reader

--- Archive_management/Boot_reading/boot.h
+++ Archive_management/Boot_reading/boot.h
@@ -1,3 +1,6 @@
+#include <string>
+#include <iostream>
+using namespace std;
 
 class boot_record
 {
@@ -9,6 +12,22 @@ private:
     int bitmap_in_clusters;
 
 public:
+
     boot_record();
     ~boot_record();
+    boot_record(FILE* file);
+    void read_boot_record(FILE* file);
+
+    void print_partition_name();
+    void print_bytes_per_sector();
+    void print_sectors_per_cluster();
+    void print_root_entry_count();
+    void print_bitmap_in_clusters();
+
+    string get_partition_name();
+    int get_bytes_per_sector();
+    int get_sectors_per_cluster();
+    int get_root_entry_count();
+    int get_bitmap_in_clusters();
+
 }__attribute__((packed));

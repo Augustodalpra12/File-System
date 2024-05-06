741f9189a0fc6ce2b363b60f6c54c83386dc46a4
gus-rautenberg <gprautenberg@gmail.com>
2024-05-06 12:36:47 -0300

    solving boot reading problem

--- Archive_management/Boot_reading/boot.cpp
+++ Archive_management/Boot_reading/boot.cpp
@@ -1,64 +1,82 @@
 #include "boot.h"
 
-boot_record::boot_record(){
-    
+boot_record::boot_record()
+{
 }
 
-boot_record::boot_record(const boot_record& boot){
+boot_record::boot_record(const boot_record &boot)
+{
     strcpy(this->partition_name, boot.partition_name);
     this->bytes_per_sector = boot.bytes_per_sector;
     this->sectors_per_cluster = boot.sectors_per_cluster;
     this->root_entry_count = boot.root_entry_count;
     this->bitmap_in_clusters = boot.bitmap_in_clusters;
 }
-boot_record::boot_record(FILE* file){
+boot_record::boot_record(FILE *file)
+{
     this->read_boot_record(file);
 }
 
-boot_record::~boot_record(){
+boot_record::~boot_record()
+{
 }
 
-void boot_record::read_boot_record(FILE* file){
+void boot_record::read_boot_record(FILE *file)
+{
     fseek(file, 0, SEEK_SET);
-    fread(this, sizeof(boot_record), 1, file);
+    fread(&this->partition_name, 8, 1, file);
+    fread(&this->bytes_per_sector, 2, 1, file);
+    fread(&this->sectors_per_cluster, 1, 1, file);
+    fread(&this->root_entry_count, 2, 1, file);
+    fread(&this->bitmap_in_clusters, 4, 1, file);
 }
 
-void boot_record::print_partition_name(){
+void boot_record::print_partition_name()
+{
     cout << "Partition name: " << this->partition_name << endl;
 }
 
-void boot_record::print_bytes_per_sector(){
+void boot_record::print_bytes_per_sector()
+{
     cout << "Bytes per sector: " << this->bytes_per_sector << endl;
 }
 
-void boot_record::print_sectors_per_cluster(){
+void boot_record::print_sectors_per_cluster()
+{
     cout << "Sectors per cluster: " << this->sectors_per_cluster << endl;
 }
 
-void boot_record::print_root_entry_count(){
+void boot_record::print_root_entry_count()
+{
     cout << "Root entry count: " << this->root_entry_count << endl;
 }
 
-void boot_record::print_bitmap_in_clusters(){
+void boot_record::print_bitmap_in_clusters()
+{
     cout << "Bitmap in clusters: " << this->bitmap_in_clusters << endl;
 }
 
-string boot_record::get_partition_name(){
+string boot_record::get_partition_name()
+{
     return this->partition_name;
 }
 
-int boot_record::get_bytes_per_sector(){
+int boot_record::get_bytes_per_sector()
+{
     return static_cast<int>(this->bytes_per_sector);
 }
 
-int boot_record::get_sectors_per_cluster(){
+int boot_record::get_sectors_per_cluster()
+{
     return static_cast<int>(this->sectors_per_cluster);
 }
 
-int boot_record::get_root_entry_count(){
+int boot_record::get_root_entry_count()
+{
     return static_cast<int>(this->root_entry_count);
 }
 
-int boot_record::get_bitmap_in_clusters(){
+int boot_record::get_bitmap_in_clusters()
+{
     return static_cast<int>(this->bitmap_in_clusters);
 }
